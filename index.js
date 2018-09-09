5662251312680e7ecd7efaac49025b5281bfec30
MarlenaFlueh <mf@esyoil.com>
2018-09-09 12:07:52 +0200

    create very basic twitter bot

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const twit = require("twit");
-const twitter = new Twit({
-  consumer_key: " ... ",
-  consumer_secret: " ... ",
-  access_token: " ... ",
-  access_token_secret: " ... "
+require("dotenv").config();
+const Twit = require("twit");
+
+const T = new Twit({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
 });
+
+T.post(
+  "statuses/update",
+  { status: "What a wonderful day!" },
+  (error, data, response) => {
+    console.log(data);
+  }
+);
