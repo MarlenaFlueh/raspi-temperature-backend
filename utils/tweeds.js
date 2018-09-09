0d92431507f578835e7c1745d70acd392e6df75f
MarlenaFlueh <mf@esyoil.com>
2018-09-09 19:50:21 +0200

    add tweets for temperature

---
+++ utils/tweeds.js
@@ -0,0 +1,20 @@
+const cold = temperature => `${temperature < 17 ? "It's cold brrrr." : ""}`;
+
+const tweeds = temperature => [
+  `In Lüneburg temperatures are round about ${temperature} degrees celsius.${
+    temperature >= 26 ? "It's so hot today." : cold(temperature)
+  }`,
+  `Hello again! We have ${temperature} degrees in Lüneburg uwu.`,
+  `Now we've got ${temperature}° celsius.`,
+  `In Lüneburg, north of Germany, temperatures are ${temperature} degrees celsius.`,
+  `What a wonderful day, we've got ${temperature}° celsius. ${cold(
+    temperature
+  )}`,
+  `Currently, we've got ${temperature} degrees celsius.`,
+  `${temperature} degrees celsius today. ${cold()}`,
+  `Cold or hot? I feel good with ${temperature} degrees celsius.`,
+  `${temperature} degrees celsius.`,
+  `${temperature}° celsius.`
+];
+
+module.exports = tweeds;
